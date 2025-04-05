3724508efecb9c36cded345e8775e690ec097877
karanveer <karenveersharma09.in>
2025-04-05 14:30:19 +0530

    new file

---
+++ loc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("enter a number");
+    scanf("%d", &a);
+
+    printf("enter a number");
+    scanf("%d", &b);
+
+    sum = a + b;
+
+    printf("the sum is :", sum);
+    return 0;
+}
